39fcbaff133eb6e3784b7756fd0ee444f1071c55
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-06-28 11:12:12 +0200

    checkOSM: split off overpassQuery function

--- src/checkOSM.js
+++ src/checkOSM.js
@@ -1,4 +1,19 @@
 const httpRequest = require('./httpRequest.js')
+const loadWikidata = require('./loadWikidata.js')
+
+function overpassQuery (query, callback) {
+  httpRequest('https://overpass-api.de/api/interpreter',
+    {
+      method: 'POST',
+      responseType: 'json',
+      body: '[out:json];' + query
+    },
+    (err, result) => {
+      if (err) { return callback(err) }
+      callback(null, result.body)
+    }
+  )
+}
 
 module.exports = function checkOSM (id, dom, callback) {
   let div = document.createElement('div')
@@ -10,15 +25,10 @@ module.exports = function checkOSM (id, dom, callback) {
   ul.className = 'check'
   div.appendChild(ul)
 
-  httpRequest('https://overpass-api.de/api/interpreter',
-    {
-      method: 'POST',
-      responseType: 'json',
-      body: '[out:json];nwr["ref:at:bda"=' + id + '];out tags;'
-    },
+  overpassQuery('nwr["ref:at:bda"=' + id + '];out tags;',
     (err, result) => {
-      if (result.body.elements.length) {
-        ul.innerHTML += '<li class="success">' + result.body.elements.length + ' Objekt via <tt>ref:at:bda=' + id + '</tt> gefunden: ' + result.body.elements.map(el => '<a target="_blank" href="https://openstreetmap.org/' + el.type + '/' + el.id + '">' + (el.tags.name || (el.type + '/' + el.id)) + '</a>').join(', ') + '</li>'
+      if (result.elements.length) {
+        ul.innerHTML += '<li class="success">' + result.elements.length + ' Objekt via <tt>ref:at:bda=' + id + '</tt> gefunden: ' + result.elements.map(el => '<a target="_blank" href="https://openstreetmap.org/' + el.type + '/' + el.id + '">' + (el.tags.name || (el.type + '/' + el.id)) + '</a>').join(', ') + '</li>'
       } else {
         ul.innerHTML += '<li class="error">Kein Eintrag mit <tt>ref:at:bda=' + id + '</tt> in der OpenStreetMap gefunden!</li>'
       }
