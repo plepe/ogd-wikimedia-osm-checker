8cfe417ab8b3e1c4d882499ce159e367c5af76a1
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-06-28 10:58:24 +0200

    Style checks

--- src/checkOSM.js
+++ src/checkOSM.js
@@ -6,6 +6,10 @@ module.exports = function checkOSM (id, dom, callback) {
 
   div.innerHTML = '<h2>OpenStreetMap</h2>'
 
+  let ul = document.createElement('ul')
+  ul.className = 'check'
+  div.appendChild(ul)
+
   httpRequest('https://overpass-api.de/api/interpreter',
     {
       method: 'POST',
@@ -14,9 +18,9 @@ module.exports = function checkOSM (id, dom, callback) {
     },
     (err, result) => {
       if (result.body.elements.length) {
-        div.innerHTML += result.body.elements.length + " Objekt gefunden: " + result.body.elements.map(el => '<a target="_blank" href="https://openstreetmap.org/' + el.type + '/' + el.id + '">' + (el.tags.name || (el.type + '/' + el.id)) + '</a>').join(', ')
+        ul.innerHTML += '<li class="success">' + result.body.elements.length + ' Objekt via <tt>ref:at:bda=' + id + '</tt> gefunden: ' + result.body.elements.map(el => '<a target="_blank" href="https://openstreetmap.org/' + el.type + '/' + el.id + '">' + (el.tags.name || (el.type + '/' + el.id)) + '</a>').join(', ') + '</li>'
       } else {
-        div.innerHTML += "Kein Eintrag in der OpenStreetMap gefunden!"
+        ul.innerHTML += '<li class="error">Kein Eintrag mit <tt>ref:at:bda=' + id + '</tt> in der OpenStreetMap gefunden!</li>'
       }
 
       callback()
