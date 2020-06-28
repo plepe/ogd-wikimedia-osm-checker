bfffd8b8105b0755042a49760d8d56076ca13038
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-06-28 11:16:45 +0200

    loadWikidata: several simultaneous requests: query only once

--- src/loadWikidata.js
+++ src/loadWikidata.js
@@ -1,5 +1,19 @@
+const callbacks = {}
+
 module.exports = function loadWikidata (id, callback) {
+  if (id in callbacks) {
+    callbacks[id].push(callback)
+    return
+  }
+
+  callbacks[id] = [callback]
+
   fetch('wikidata.cgi?id=' + id)
     .then(res => res.json())
-    .then(result => callback(null, result))
+    .then(result => {
+      let cbs = callbacks[id]
+      delete callbacks[id]
+
+      cbs.forEach(cb => cb(null, result))
+    })
 }
