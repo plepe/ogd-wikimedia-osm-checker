f08fb7b5c8a579e48e0851fb90642043fe48b41f
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2020-06-29 10:23:03 +0200

    In internal server, fake cgi execution

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const http = require('http')
 const fs = require('fs')
 const queryString = require('query-string')
 
+const cgi = {
+  commons: require('./server/commons.js'),
+  wikidata: require('./server/wikidata.js')
+}
+
 contentTypes = {
   html: 'text/html',
   css: 'text/css',
@@ -11,13 +16,14 @@ contentTypes = {
 
 const requestListener = function (req, res) {
   let file
+  let proc
   let ext = 'html'
   let param = {}
 
   if (req.url === '/') {
     file = '/index.html'
   } else {
-    let m = req.url.match(/^(\/[\/[0-9A-Z]+\.([A-Z]+))(\?.*|)$/i)
+    let m = req.url.match(/^(\/([\/[0-9A-Z]+)\.([A-Z]+))(\?.*|)$/i)
     if (!m) {
       res.writeHead(500)
       res.end('Invalid request')
@@ -25,8 +31,31 @@ const requestListener = function (req, res) {
     }
 
     file = m[1]
-    ext = m[2]
-    param = queryString.parse(m[3])
+    proc = m[2]
+    ext = m[3]
+    param = queryString.parse(m[4])
+  }
+
+  if (ext === 'cgi') {
+    if (!(proc in cgi)) {
+      res.writeHead(404)
+      res.end('File not found')
+      return console.error('Process ' + proc + ' not found')
+    }
+
+    cgi[proc](param, (err, result) => {
+      if (err) {
+        res.writeHead(500)
+        res.end()
+        return console.error(err)
+      }
+
+      res.setHeader("Content-Type", 'application/json')
+      res.writeHead(200)
+      res.end(JSON.stringify(result, null, '  '))
+    })
+
+    return
   }
 
   fs.readFile(__dirname + file, (err, contents) => {
