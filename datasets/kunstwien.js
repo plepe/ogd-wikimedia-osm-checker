e11fdc96b79625b360d6a83765a47798402f5235
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-07-05 18:48:31 +0200

    kunstwien: show entry

--- datasets/kunstwien.js
+++ datasets/kunstwien.js
@@ -43,9 +43,32 @@ module.exports = {
   },
 
   showEntry (data, dom) {
-    const pre = document.createElement('pre')
-    dom.appendChild(pre)
+    const div = document.createElement('div')
+    dom.appendChild(div)
 
-    pre.appendChild(document.createTextNode(JSON.stringify(data, null, '  ')))
+    div.innerHTML = '<h2>Stadt Wien</h2>'
+
+    const ul = document.createElement('ul')
+    div.appendChild(ul)
+
+    ul.innerHTML += '<li>ID: ' + data.ID + '</li>'
+    ul.innerHTML += '<li>Titel: ' + escHTML(data.OBJEKTTITEL) + '</li>'
+    ul.innerHTML += '<li>Vulgonamen: ' + escHTML(data.VULGONAMEN) + '</li>'
+    ul.innerHTML += '<li>Typ: ' + escHTML(data.TYP) + '</li>'
+    ul.innerHTML += '<li>Adresse: ' + escHTML(data.STRASSE) + ', ' + escHTML(data.PLZ) + ' ' + escHTML(data.ORT) + '</li>'
+    let coords = data.SHAPE.match(/POINT \((-?\d+\.\d+) (-?\d+\.\d+)\)/)
+    if (coords) {
+      ul.innerHTML += '<li>Koordinaten: <a target="_blank" href="https://openstreetmap.org/?mlat=' + coords[2] + '&mlon=' + coords[1] + '#map=19/' + coords[2] + '/' + coords[1] + '">' + parseFloat(coords[2]).toFixed(5) + ', ' + parseFloat(coords[1]).toFixed(5) + '</a></li>'
+    }
+    ul.innerHTML += '<li>Standort: ' + escHTML(data.STANDORT) + '</li>'
+    ul.innerHTML += '<li>Beschreibung: ' + escHTML(data.BESCHREIBUNG) + '</li>'
+    ul.innerHTML += '<li>Geschichte: ' + escHTML(data.GESCHICHTE) + '</li>'
+    ul.innerHTML += '<li>Entstehung: ' + escHTML(data.ENTSTEHUNG) + ' (' + escHTML(data.EPOCHE) + ')</li>'
+    ul.innerHTML += '<li>Künstler*in: ' + escHTML(data.KUENSTLER) + '</li>'
+    ul.innerHTML += '<li>Material: ' + escHTML(data.MATERIAL) + '</li>'
+
+    //const pre = document.createElement('pre')
+    //dom.appendChild(pre)
+    //pre.appendChild(document.createTextNode(JSON.stringify(data, null, '  ')))
   }
 }
