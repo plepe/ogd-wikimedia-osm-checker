64613a234babcd5a0add94ebe0047103e77e8912
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-07-07 20:31:26 +0200

    lint

--- test/parseMWTemplate.js
+++ test/parseMWTemplate.js
@@ -3,19 +3,19 @@ const assert = require('assert')
 const parseMWTemplate = require('../src/parseMWTemplate.js')
 
 describe('parseMWTemplate', () => {
-  it ('single line', () => {
-    let result = parseMWTemplate('{{doo|2345}}', 'doo')
+  it('single line', () => {
+    const result = parseMWTemplate('{{doo|2345}}', 'doo')
 
-    assert.deepEqual(result, [{'1': '2345'}])
+    assert.deepEqual(result, [{ 1: '2345' }])
   })
-  it ('single line, multiple templates', () => {
-    let result = parseMWTemplate('{{doo|2345|8765}}\n{{Doo|1=1234|2=2345}}', 'doo')
+  it('single line, multiple templates', () => {
+    const result = parseMWTemplate('{{doo|2345|8765}}\n{{Doo|1=1234|2=2345}}', 'doo')
 
-    assert.deepEqual(result, [{'1': '2345', 2:'8765'}, {'1': '1234', 2: '2345'}])
+    assert.deepEqual(result, [{ 1: '2345', 2: '8765' }, { 1: '1234', 2: '2345' }])
   })
-  it ('multiple line', () => {
-    let result = parseMWTemplate('{{doo\n | foo = 2345\n | bar = text with whitespace }}\n{{Doo|1=1234}}', 'doo')
+  it('multiple line', () => {
+    const result = parseMWTemplate('{{doo\n | foo = 2345\n | bar = text with whitespace }}\n{{Doo|1=1234}}', 'doo')
 
-    assert.deepEqual(result, [{foo: '2345', bar: 'text with whitespace'}, {'1': '1234'}])
+    assert.deepEqual(result, [{ foo: '2345', bar: 'text with whitespace' }, { 1: '1234' }])
   })
 })
