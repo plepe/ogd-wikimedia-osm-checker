eb3cc605878d85cf7ea2aed2cd80d5d782a07908
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-07-07 23:45:57 +0200

    parseMWTemplate: improvements (allow nested templates, ...)

--- src/parseMWTemplate.js
+++ src/parseMWTemplate.js
@@ -1,8 +1,44 @@
+const regexKey = new RegExp('^\\s*([^=|{}\\[\\]]*)=', '')
+const regexNextField = new RegExp('\\|', 'm')
+const regexEnd = new RegExp('\}\}', 'm')
+const regexNest = new RegExp('(\\[\\[|\\{\\{)', 'm')
+
+function findNested (str) {
+  const currentNest = str.match(/^(\{\{|\[\[)(.*)/)
+  if (!currentNest) {
+    return 0
+  }
+
+  let length = currentNest[1].length
+  str = str.substr(length)
+
+  let regexEndNest
+  if (currentNest[1] === '{{') {
+    regexEndNest = new RegExp('\\}\\}', 'm')
+  } else if (currentNest[1] === '[[') {
+    regexEndNest = new RegExp('\\]\\]', 'm')
+  }
+
+  let mNest = str.match(regexNest)
+  let mEnd = str.match(regexEndNest)
+  while (mNest && (!mEnd || mNest.index < mEnd.index)) {
+    str = str.substr(mNest.index)
+    const l = findNested(str)
+    length += mNest.index + l
+    str = str.substr(l)
+
+    mNest = str.match(regexNest)
+    mEnd = str.match(regexEndNest)
+  }
+
+  length += mEnd.index + mEnd[0].length
+  return length
+}
+
 module.exports = function parseMWTemplate (str, templateId) {
   const results = []
 
-  const regexStart = new RegExp('(\{\{' + templateId + ')\s*', 'i')
-  const regexEnd = new RegExp('\}\}')
+  const regexStart = new RegExp('(\\{\\{\\s*' + templateId + '\\s*(\\||\\}\\}))', 'im')
   let m = str.match(regexStart)
   while (m) {
     const d = {}
@@ -13,29 +49,51 @@ module.exports = function parseMWTemplate (str, templateId) {
       str = str.substr(1)
     }
 
-    m = str.match(regexEnd)
-    templateText = str.substr(0, m.index)
+    if (m[2] === '}}') {
+      results.push(d)
+      m = str.match(regexStart)
+      continue
+    }
 
-    const rows = templateText.split(/\|/g)
-    rows.forEach(row => {
-      const mw = row.match(/^(.*)\n\s*$/m)
-      if (mw) {
-        row = mw[1]
-      }
-      if (row.match(/^\s*$/)) {
-        return
-      }
+    let key = null
+    let value = ''
+    let done = false
+    while (!done) {
+      const mKey = str.match(regexKey)
+      const mNest = str.match(regexNest)
+      const mNext = str.match(regexNextField)
+      const mEnd = str.match(regexEnd)
 
-      const m1 = row.match(/^([^=]+)=(.*)$/i)
-      if (m1) {
-        d[m1[1].trim()] = m1[2].trim()
+      if (value === '' && mKey) {
+        key = mKey[1].trim()
+        str = str.substr(mKey[0].length)
+      } else if (mNext && (!mNest || mNext.index < mNest.index) && (!mEnd || mNext.index < mEnd.index)) {
+        value += str.substr(0, mNext.index)
+        str = str.substr(mNext.index + 1)
+        if (value !== '' || key) {
+          d[key === null ? numIndex : key] = value.trim()
+        }
+
+        numIndex++
+        key = null
+        value = ''
+      } else if (mNest && (!mEnd || mNest.index < mEnd.index)) {
+        const length = findNested(str.substr(mNest.index))
+        value += str.substr(0, mNest.index + length)
+        str = str.substr(mNest.index + length)
+      } else if (mEnd) {
+        value += str.substr(0, mEnd.index)
+        str = str.substr(mEnd.index + 2)
+        if (value !== '' || key) {
+          d[key === null ? numIndex : key] = value.trim()
+        }
+        results.push(d)
+        done = true
       } else {
-        d[numIndex++] = row.trim()
+        done = true
       }
-    })
-    results.push(d)
+    }
 
-    str = str.substr(m.index + 2)
     m = str.match(regexStart)
   }
 
--- test/parseMWTemplate.js
+++ test/parseMWTemplate.js
@@ -3,6 +3,11 @@ const assert = require('assert')
 const parseMWTemplate = require('../src/parseMWTemplate.js')
 
 describe('parseMWTemplate', () => {
+  it('without value', () => {
+    const result = parseMWTemplate('{{doo}} {{foo}} {{ doo }}', 'doo')
+
+    assert.deepEqual(result, [{}, {}])
+  })
   it('single line', () => {
     const result = parseMWTemplate('{{doo|2345}}', 'doo')
 
@@ -17,5 +22,15 @@ describe('parseMWTemplate', () => {
     const result = parseMWTemplate('{{doo\n | foo = 2345\n | bar = text with whitespace }}\n{{Doo|1=1234}}', 'doo')
 
     assert.deepEqual(result, [{ foo: '2345', bar: 'text with whitespace' }, { 1: '1234' }])
+  })
+  it('nested templates', () => {
+    const result = parseMWTemplate('{{doo\n | foo = 2345\n | bar = text with whitespace and {{another template}} embedded | even more complex \n [[e|f]] {{foo|bar}} stuff {{a{{b}} }} }}\n{{Doo|1=1234}}', 'doo')
+
+    assert.deepEqual(result, [{ foo: '2345', bar: 'text with whitespace and {{another template}} embedded', 3: 'even more complex \n [[e|f]] {{foo|bar}} stuff {{a{{b}} }}' }, { 1: '1234' }])
+  })
+  it('error', () => {
+    const result = parseMWTemplate('{{doo}} {{foo}} {{ doo }} {{doo|foo=|\nbar=}} {{ doo [[a|b=]] ', 'doo')
+
+    assert.deepEqual(result, [{}, {},{foo:'', bar:''}])
   })
 })
