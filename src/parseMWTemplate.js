a6d9f918cbf02e5c00be1922c1633e6884fb46d3
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-07-07 07:18:14 +0200

    parseMWTemplate: read single-line templates too and without field name

--- src/parseMWTemplate.js
+++ src/parseMWTemplate.js
@@ -1,27 +1,36 @@
 module.exports = function parseMWTemplate (str, templateId) {
   let results = []
 
-  let regexStart = new RegExp('(\{\{' + templateId + ')', 'i')
+  let regexStart = new RegExp('(\{\{' + templateId + ')\s*', 'i')
   let regexEnd = new RegExp('\}\}')
   let m = str.match(regexStart)
   while (m) {
     let d = {}
+    let numIndex = 1
 
-    str = str.substr(m.index + m[1].length + 1)
+    str = str.substr(m.index + m[1].length)
+    if (str.substr(0, 1) === '\n') {
+      str = str.substr(1)
+    }
+
     m = str.match(regexEnd)
     templateText = str.substr(0, m.index)
 
-    let rows = templateText.split(/\n\|/g)
+    let rows = templateText.split(/\|/g)
     rows.forEach(row => {
-      if (row.substr(-1) === '\n') {
-        row = row.substr(0, row.length - 1)
+      let mw = row.match(/^(.*)\n\s*$/m)
+      if (mw) {
+        row = mw[1]
+      }
+      if (row.match(/^\s*$/)) {
+        return
       }
 
       let m1 = row.match(/^([^=]+)=(.*)$/i)
       if (m1) {
         d[m1[1].trim()] = m1[2].trim()
       } else {
-        console.log('cant parse', row)
+        d[numIndex++] = row.trim()
       }
     })
     results.push(d)
