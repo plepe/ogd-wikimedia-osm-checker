769c704d3c97b93be89265222a4b267b5a074925
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-07-30 11:48:24 +0200

    update-docker: expose log directory

--- update-docker.sh
+++ update-docker.sh
@@ -2,4 +2,4 @@
 git pull
 docker build -t skunk/ogd-wikimedia-osm-checker .
 docker container rm -f checker
-docker run --name checker --restart=always -p 127.0.0.1:43210:8080 -d skunk/ogd-wikimedia-osm-checker
+docker run --name checker --restart=always -p 127.0.0.1:43210:8080 -v /home/skunk/code/ogd-wikimedia-osm-checker/log:/usr/src/app/log -d skunk/ogd-wikimedia-osm-checker
