e14776e5544cde2f461409dc1060c969b94aecbe
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-07-30 11:47:25 +0200

    unfin

--- update.js
+++ update.js
@@ -61,14 +61,37 @@ function downloadKunstwerkeWien (callback) {
     })
 }
 
-downloadBda(err => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-})
+function downloadWienGeschichteWiki (callback) {
+  const data = []
 
-downloadKunstwerkeWien(err => {
+  fetch('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:KULTURWIKIOGD%20&srsName=EPSG:4326&outputFormat=csv')
+    .then(response => {
+      csvtojson({ delimiter: ',' })
+        .fromStream(response.body)
+        .subscribe(line => {
+          data.push(line)
+        })
+        .on('done', () => {
+          fs.writeFile('data/wien-geschichte-wiki.json', JSON.stringify(data, null, '  '), callback)
+        })
+    })
+}
+
+//downloadBda(err => {
+//  if (err) {
+//    console.error(err)
+//    process.exit(1)
+//  }
+//})
+//
+//downloadKunstwerkeWien(err => {
+//  if (err) {
+//    console.error(err)
+//    process.exit(1)
+//  }
+//})
+
+downloadWienGeschichteWiki(err => {
   if (err) {
     console.error(err)
     process.exit(1)
