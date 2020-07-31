938ed8667b555efc5a45390ca7f8540a755917fc
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-07-31 07:03:29 +0200

    CheckOsmLoadFromWikidata: rewrite

--- checks/CheckOsmLoadFromWikidata.js
+++ checks/CheckOsmLoadFromWikidata.js
@@ -9,12 +9,6 @@ class CheckOsmLoadFromWikidata extends Check {
   check (ob) {
     let wikidataId
 
-    // OSM object has been loaded by 'osmLoadSimilar'. When re-showing data, let
-    // that module do it.
-    if (ob.osmSimilar) {
-      return
-    }
-
     if (!ob.data.wikidata && !ob.data.osm) {
       // wait for wikidata info to be loaded
       return
@@ -25,23 +19,27 @@ class CheckOsmLoadFromWikidata extends Check {
       return
     }
 
+    const loadingWikidata = ob.data.wikidata.filter(entry => !ob.load('osm', 'nwr[wikidata="' + entry.id + '"];'))
+
+    if (loadingWikidata.length) {
+      return false
+    }
+
     if (ob.data.wikidata.length) {
       wikidataId = ob.data.wikidata[0].id
     }
 
-    if (!ob.data.osm) {
-      let loading = ob.data.wikidata.filter(entry => !ob.load('osm', 'nwr[wikidata="' + entry.id + '"];'))
-      if (loading.length) {
-        return
-      }
+    let refWdResult = []
+    if (wikidataId) {
+      refWdResult = ob.data.osm.filter(el => el.tags.wikidata === wikidataId)
     }
 
-    const results = ob.data.osm.filter(el => el.tags.wikidata === wikidataId)
-    if (results.length) {
-      return ob.message('osm', STATUS.SUCCESS, results.length + ' Objekt via <tt>wikidata=' + wikidataId + '</tt> gefunden: <ul>' + results.map(el => '<li>' + osmFormat(el, ob) + '</li>').join('') + '</ul>')
+    if (refWdResult.length) {
+      ob.message('osm', STATUS.SUCCESS, refWdResult.length + ' Objekt via <tt>wikidata=' + wikidataId + '</tt> gefunden: ' + refWdResult.map(el => '<li>' + osmFormat(el, ob) + '</li>').join('') + '</ul>')
+      return true
     }
 
-    return ob.message('osm', STATUS.ERROR, 'Kein Eintrag <tt>wikidata=' + wikidataId + '</tt> in der OpenStreetMap gefunden!')
+    return ob.message('osm', STATUS.ERROR, 'Kein Eintrag mit <tt>wikidata=' + wikidataId + '</tt> in der OpenStreetMap gefunden!')
   }
 }
 
