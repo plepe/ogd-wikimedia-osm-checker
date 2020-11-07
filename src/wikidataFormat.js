35bfccadad7d878c93316cc4ba2cc59200b685f9
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-11-07 20:59:02 +0100

    wikidataFormat: link certain properties

--- src/wikidataFormat.js
+++ src/wikidataFormat.js
@@ -2,6 +2,19 @@ const forEach = require('foreach')
 
 const printAttrList = require('./printAttrList.js')
 
+const links = {
+  P18: function (value) {
+    return 'https://commons.wikimedia.org/wiki/File:' + value.mainsnak.datavalue.value.replace(/ /g, '_')
+  },
+  P373: function (value) {
+    return 'https://commons.wikimedia.org/wiki/Category:' + value.mainsnak.datavalue.value.replace(/ /g, '_')
+  },
+  P625: function (value) {
+    const coords = value.mainsnak.datavalue.value
+    return 'https://openstreetmap.org/?mlat=' + coords.latitude + '&mlon=' + coords.longitude + '#map=19/' + coords.latitude + '/' + coords.longitude + '">' + coords.latitude + ', ' + coords.longitude
+  }
+}
+
 function label (labels) {
   let lang = ['de-at', 'de', 'en'].filter(l => labels[l])
   if (!lang.length) {
@@ -28,14 +41,20 @@ module.exports = function wikidataFormat (ob) {
   })
 
   forEach(ob.claims, (values, key) => {
-    attrList.push({
+    const attr = {
       key: key,
       title: ob.claimsTitle[key],
       value: values,
-      text: values
-        .map((value) => value.text)
-        .join(', ')
-    })
+      text: values.map(value => value.text)
+    }
+
+    if (key in links) {
+      attr.link = values.map(value => links[key](value))
+    } else if (values[0].mainsnak.datavalue.value['entity-type'] === 'item') {
+      attr.link = values.map(value => 'https://wikidata.org/wiki/' + value.mainsnak.datavalue.value.id)
+    }
+
+    attrList.push(attr)
   })
 
   return printAttrList(attrList) // + '<pre>' + JSON.stringify(ob, null, '  ') + '</pre>'
