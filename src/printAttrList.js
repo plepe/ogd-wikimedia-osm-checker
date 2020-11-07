ba107e82c16744119e7407b572762902716d3760
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-11-07 20:46:59 +0100

    printAttrList: prepare multi-value entries

--- src/printAttrList.js
+++ src/printAttrList.js
@@ -1,24 +1,32 @@
 const escHTML = require('html-escape')
 
+function formatValue (entry) {
+  let title = ''
+  if (entry.value) {
+    title = ' title="' + escHTML(typeof entry.value === 'object' ? JSON.stringify(entry.value, null, '  ') : entry.value) + '"'
+  }
+
+  let tagStart = 'span'
+  let tagEnd = 'span'
+  if (entry.link) {
+    tagStart = 'a target="_blank" href="' + escHTML(entry.link) + '"'
+    tagEnd = 'a'
+  }
+
+  return '<' + tagStart + title + ' class="value">' + escHTML(entry.text) + '</' + tagEnd + '>'
+}
+
 module.exports = function (list) {
   let result = '<ul class="attrList">'
 
   list.forEach(entry => {
     if (!entry) { return }
 
-    let title = ''
-    if (entry.value) {
-      title = ' title="' + escHTML(typeof entry.value === 'object' ? JSON.stringify(entry.value, null, '  ') : entry.value) + '"'
-    }
+    result += '<li><span' + (entry.key ? ' title="' + escHTML(entry.key) + '"' : '') + ' class="key">' + escHTML(entry.title) + '</span>: '
 
-    let tagStart = 'span'
-    let tagEnd = 'span'
-    if (entry.link) {
-      tagStart = 'a target="_blank" href="' + escHTML(entry.link) + '"'
-      tagEnd = 'a'
-    }
+    result += formatValue(entry)
 
-    result += '<li><span' + (entry.key ? ' title="' + escHTML(entry.key) + '"' : '') + ' class="key">' + escHTML(entry.title) + '</span>: <' + tagStart + title + ' class="value">' + escHTML(entry.text) + '</' + tagEnd + '></li>\n'
+    result += '</li>'
   })
 
   result += '</ul>\n'
