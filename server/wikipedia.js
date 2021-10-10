40ede132e6af37c66636a3854eae53b065c80a35
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-10-10 22:08:24 +0200

    Server/Wikipedia: wikipedia-list-extractor now uses YAML config

--- server/wikipedia.js
+++ server/wikipedia.js
@@ -1,16 +1,17 @@
 const fs = require('fs')
 const WikipediaListExtractor = require('wikipedia-list-extractor')
+const yaml = require('yaml')
 
 const lists = {}
 const extractorOptions = {loadRendered: true}
 
 module.exports = function (options, callback) {
   if (!(options.list in lists)) {
-    fs.readFile('node_modules/wikipedia-list-extractor/data/' + options.list + '.json',
+    fs.readFile('node_modules/wikipedia-list-extractor/data/' + options.list + '.yaml',
       (err, file) => {
         if (err) { return callback(err) }
 
-        const def = JSON.parse(file)
+        const def = yaml.parse(file.toString())
         lists[options.list] = new WikipediaListExtractor(options.list, def)
 
         lists[options.list].get(options.id, extractorOptions, callback)
