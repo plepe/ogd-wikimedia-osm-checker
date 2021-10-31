4b129d9ec982da2f3596e34bda05f3691bfbedc4
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-10-31 13:22:50 +0100

    Wiener Wohnen: improve parse script

--- update.js
+++ update.js
@@ -68,25 +68,42 @@ function downloadWienerWohnen (callback) {
     .then(response => response.json())
     .then(body => {
       const data = body.aaData.map(entry => {
-        let m1 = entry[1].match(/^<a href='(\/hof\/([0-9]+)\/.*)'>(.*)<\/a>$/)
-        let m3 = entry[3].match(/^([0-9]{4})-([0-9]{4})$/)
+        const m1 = entry[1].match(/^<a href='(\/hof\/([0-9]+)\/.*)'>(.*)<\/a>$/)
+        const m2 = entry[2].match(/^(1[0-9][0-9].)? ([^,]*), (.*)$/)
+        const m3 = entry[3].match(/^([0-9]{4})-([0-9]{4})$/)
+        const m5 = entry[5].match(/^(?:Architekt: ([^<]*)<br\/>)?(?:Wohnungen: ([0-9]+)<br\/>)?(?:Lokale: ([0-9]+))?/)
 
         if (!m1) {
           console.log(entry[1])
           return
         }
 
-        return {
+        const result = {
           id: m1[2],
           image: entry[0].match(/^<img src='([^']*)'/)[1],
           url: m1[1],
           name: m1[3],
-          address: entry[2],
-          startYear: m3 ? m3[1] : entry[3],
-          endYear: m3 ? m3[2] : entry[3],
-          pdf: entry[4].match(/^<a href='([^']*)'/)[1],
-          description: entry[5]
+          plz: m2[1] ? m2[1] : 'other',
+          ort: m2[2],
+          address: m2[3],
+          constructionStartYear: m3 ? m3[1] : entry[3],
+          constructionEndYear: m3 ? m3[2] : entry[3],
+          pdf: entry[4].match(/^<a href='([^']*)'/)[1]
         }
+
+        if (m5[1]) {
+          result.architects = m5[1]
+        }
+
+        if (m5[2]) {
+          result.flats = parseInt(m5[2])
+        }
+
+        if (m5[3]) {
+          result.commercialSpaces = parseInt(m5[3])
+        }
+
+        return result
       })
 
       fs.writeFile('data/wiener_wohnen.json', JSON.stringify(data, null, '  '), callback)
