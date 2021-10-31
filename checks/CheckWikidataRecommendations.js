6bc4ff940528016f1ded39a2baff0357b29f836f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-10-31 13:24:29 +0100

    CheckWikidataRecommendations: more properties

--- checks/CheckWikidataRecommendations.js
+++ checks/CheckWikidataRecommendations.js
@@ -3,11 +3,14 @@ const Check = require('../src/Check.js')
 
 const properties = {
   P84: 'Architekt_in',
+  P138: 'benannt nach',
   P170: 'Urheber_in',
   P186: 'Material',
+  P571: 'Entstehung',
   P580: 'Startzeitpunkt',
   P417: 'Patron_in',
   P180: 'Motiv',
+  P1619: 'Eröffnung',
   P1684: 'Inschrift',
   P7842: 'Wien Geschichte Wiki ID',
   P8231: 'Wiener Wohnen ID'
