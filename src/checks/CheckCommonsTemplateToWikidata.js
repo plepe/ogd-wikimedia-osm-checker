69c801547448e13e6c05c551f05a3c28a393295b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-11-20 14:07:22 +0100

    Remove duplicate code

--- src/checks/CheckCommonsTemplateToWikidata.js
+++ src/checks/CheckCommonsTemplateToWikidata.js
@@ -12,26 +12,6 @@ class CheckCommonsTemplateToWikidata extends Check {
       return true
     }
 
-    const files = ob.data.commons.filter(page => page.title.match(/^File:/))
-    const categories = ob.data.commons.filter(page => page.title.match(/^Category:/))
-    if (files.length) {
-      ob.message('commons', STATUS.SUCCESS, files.length + ' Bild(er) gefunden, die auf das Objekt verweisen: ' + files.map((page, i) => '<a target="_blank" href="https://commons.wikimedia.org/wiki/' + escHTML(page.title) + '">#' + (i + 1) + '</a>').join(', ') + '.')
-    }
-
-    if (categories.length) {
-      ob.message('commons', STATUS.SUCCESS, categories.length + ' Kategorie(n) gefunden, die auf das Objekt verweisen: ' + categories.map((page, i) => '<a target="_blank" href="https://commons.wikimedia.org/wiki/' + escHTML(page.title) + '">#' + (i + 1) + '</a>').join(', ') + '.')
-    } else {
-      if (files.length === 0) {
-        ob.message('commons', STATUS.ERROR, 'Weder Bilder noch Kategorien gefunden, die auf dieses Objekt verweisen.')
-      } else {
-        ob.message('commons', STATUS.WARNING, 'Keine Kategorie gefunden, die auf das Objekt verweist.')
-      }
-
-      if (!ob.data.wikidata) {
-        ob.data.wikidata = []
-      }
-    }
-
     // if we couldn't find a wikidata item, try via category
     if (ob.data.wikidata && ob.data.wikidata.length === 0) {
       ob.data.commons.forEach(page => {
