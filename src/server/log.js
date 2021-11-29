b4f8e971ae5439143003de81599d3c08167a7af2
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-11-29 22:08:47 +0100

    server/log: on startup, create log directory

--- src/server/log.js
+++ src/server/log.js
@@ -1,5 +1,13 @@
 const fs = require('fs')
 
+const path = 'log'
+
+fs.mkdir(path, (err) => {
+  if (err && err.code !== 'EEXIST') {
+    console.error("Can't create log directory", err.toString())
+  }
+})
+
 module.exports = function (options, callback) {
-  fs.appendFile('log/access.log', new Date().toISOString() + ' ' + options.path + '\n', callback)
+  fs.appendFile(path + '/access.log', new Date().toISOString() + ' ' + options.path + '\n', callback)
 }
