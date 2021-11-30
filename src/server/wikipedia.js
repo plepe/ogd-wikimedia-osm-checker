4ac53528b70d116d1323678ca18034972bbe083b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-11-30 16:35:09 +0100

    Server/wikipedia: don't need to load wikipedia list config

--- src/server/wikipedia.js
+++ src/server/wikipedia.js
@@ -7,18 +7,7 @@ const extractorOptions = { loadRendered: false }
 
 module.exports = function (options, callback) {
   if (!(options.list in lists)) {
-    fs.readFile('node_modules/wikipedia-list-extractor/data/' + options.list + '.yaml',
-      (err, file) => {
-        if (err) { return callback(err) }
-
-        const def = yaml.parse(file.toString())
-        lists[options.list] = new WikipediaListExtractor(options.list, def)
-
-        lists[options.list].get(options.id, extractorOptions, callback)
-      }
-    )
-
-    return
+    lists[options.list] = new WikipediaListExtractor(options.list)
   }
 
   if (options.reload) {
