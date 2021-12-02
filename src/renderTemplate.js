80d334f9555943d396779e2a49bd3afd6b18acb2
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-12-02 10:49:44 +0100

    addTags/recommendTags/...: also support array of values

--- src/renderTemplate.js
+++ src/renderTemplate.js
@@ -3,6 +3,18 @@ const twig = require('twig').twig
 const twigTemplates = {}
 
 module.exports = function renderTemplate (template, data) {
+  if (Array.isArray(template)) {
+    return template
+      .map(line => {
+        if (!(line in twigTemplates)) {
+          twigTemplates[line] = twig({ data: line })
+        }
+
+        return twigTemplates[line].render(data)
+      })
+      .filter(f => f)
+  }
+
   template = template.replace(/\n/g, '\n\n')
   if (!(template in twigTemplates)) {
     twigTemplates[template] = twig({ data: template })
