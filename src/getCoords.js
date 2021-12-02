b4f57478fa747010e85c953895557d1ead681ab1
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-12-02 22:23:21 +0100

    getCoords: 'id' could be a twig template

--- src/getCoords.js
+++ src/getCoords.js
@@ -1,17 +1,33 @@
+const twig = require('twig').twig
+
+const twigTemplates = {}
+
 module.exports = function getCoords (data, field) {
   if (!field) {
     return null
   }
 
+  let geometry
+  if (field.id) {
+    if (field.id.match(/\{/)) {
+      if (!(field.id in twigTemplates)) {
+        twigTemplates[field.id] = twig({ data: field.id })
+      }
+
+      geometry = twigTemplates[field.id].render({ item: data })
+    } else {
+      geometry = data[field.id]
+    }
+  }
+
   if (field.type === 'wkt') {
-    const m = data[field.id].match(/POINT \((-?\d+\.\d+) (-?\d+\.\d+)\)/)
+    const m = geometry.match(/POINT \((-?\d+\.\d+) (-?\d+\.\d+)\)/)
     return { latitude: m[2], longitude: m[1] }
   }
 
   if (field.type === 'geojson') {
-    const m = data[field.id]
-    if (m.type === 'Point') {
-      return { latitude: m.coordinates[1], longitude: m.coordinates[0] }
+    if (geometry.type === 'Point') {
+      return { latitude: geometry.coordinates[1], longitude: geometry.coordinates[0] }
     }
   }
 }
