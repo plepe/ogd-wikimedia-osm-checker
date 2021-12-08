cdcdb9461a04801d6b576abaa3cdcd181ddd5088
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-12-08 21:01:44 +0100

    move node specific code to src/node.js

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ const fs = require('fs')
 const path = require('path')
 const queryString = require('query-string')
 
-global.XMLHttpRequest = require('w3c-xmlhttprequest').XMLHttpRequest
-global.fetch = require('node-fetch')
-const jsdom = require('jsdom')
-const { JSDOM } = jsdom
-const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>')
-global.document = dom.window.document
+require('./src/node')
 
 const datasetsList = require('./src/datasetsList')
 const cgi = require('./src/server/index')
