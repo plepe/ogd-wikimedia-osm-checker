5fe7235ff13a1b7c4348681b2b171bc5a66222df
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-12-12 20:04:21 +0100

    datasetsList: Move yaml parsing to server-side to remove dependencies

--- src/datasetsList.js
+++ src/datasetsList.js
@@ -1,9 +1,5 @@
-const async = require('async')
 const fs = require('fs')
-const yaml = require('yaml')
 const queryString = require('query-string')
-
-const loadFile = require('./loadFile')
 
 module.exports = function datasetsList (options = {}, callback) {
   if (fs.readFile) {
@@ -21,19 +17,7 @@ module.exports = function datasetsList (options = {}, callback) {
         })
         .filter(file => file)
 
-      if (options.withContent) {
-        async.map(files, (d, done) => {
-          loadFile('datasets/' + d.id + '.yaml', (err, body) => {
-            if (err) { return done(err) }
-
-            const c = yaml.parse(body.toString())
-            c.id = d.id
-            done(null, c)
-          })
-        }, callback)
-      } else {
-        callback(null, files)
-      }
+      callback(null, files)
     })
   } else {
     let param = ''
--- src/server/datasets.js
+++ src/server/datasets.js
@@ -1,5 +1,27 @@
+const async = require('async')
+const yaml = require('yaml')
+
 const datasetsList = require('../datasetsList')
+const loadFile = require('../loadFile')
 
 module.exports = function (options, callback) {
-  datasetsList(options, callback)
+  datasetsList(options,
+    (err, list) => {
+      if (err) { return callback(err) }
+
+      if (options.withContent) {
+        async.map(files, (d, done) => {
+          loadFile('datasets/' + d.id + '.yaml', (err, body) => {
+            if (err) { return done(err) }
+
+            const c = yaml.parse(body.toString())
+            c.id = d.id
+            done(null, c)
+          })
+        }, callback)
+      } else {
+        return callback(null, list)
+      }
+    }
+  )
 }
