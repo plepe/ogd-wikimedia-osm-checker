276450157a7a2d2df924a6ec233284b2e6e1cac8
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-03-16 18:53:13 +0100

    loader-osm: create separate function

--- src/loader-osm.js
+++ src/loader-osm.js
@@ -1,21 +1,23 @@
 const httpRequest = require('./httpRequest.js')
 
-module.exports = {
-  load (queries, options, callback) {
-    const body = '[out:json];(' + queries.join('') + ');out tags bb;'
+function load (queries, options, callback) {
+  const body = '[out:json];(' + queries.join('') + ');out tags bb;'
 
-    httpRequest('https://overpass-api.de/api/interpreter',
-      {
-        method: 'POST',
-        responseType: 'json',
-        body
-      },
-      (err, result) => {
-        if (err) { return callback(err) }
-        callback(null, result.body.elements)
-      }
-    )
-  },
+  httpRequest('https://overpass-api.de/api/interpreter',
+    {
+      method: 'POST',
+      responseType: 'json',
+      body
+    },
+    (err, result) => {
+      if (err) { return callback(err) }
+      callback(null, result.body.elements)
+    }
+  )
+}
+
+module.exports = {
+  load,
 
   includes (arr, el) {
     return !!arr.filter(e => e.type === el.type && e.id === el.id).length
