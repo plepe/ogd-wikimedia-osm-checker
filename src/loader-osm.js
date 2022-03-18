a6ed6dae48a9dc3262351cfcf805cfae64930118
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-03-18 21:16:21 +0100

    loader-osm: minimum of 2 sec between overpass requests

--- src/loader-osm.js
+++ src/loader-osm.js
@@ -1,6 +1,25 @@
 const httpRequest = require('./httpRequest.js')
 
+let active = null
+const queue = []
+const delay = 2000
+
 function load (queries, options, callback) {
+  queue.push([queries, options, callback])
+
+  if (!active) {
+    next()
+  }
+}
+
+function next () {
+  if (!queue.length) {
+    return
+  }
+
+  const [queries, options, callback] = queue.shift()
+  active = true
+
   const body = '[out:json];(' + queries.join('') + ');out tags bb;'
 
   httpRequest('https://overpass-api.de/api/interpreter',
@@ -10,6 +29,11 @@ function load (queries, options, callback) {
       body
     },
     (err, result) => {
+      global.setTimeout(() => {
+        active = false
+        next()
+      }, delay)
+
       if (err) { return callback(err) }
       callback(null, result.body.elements)
     }
