d8fdbfc03c264f1f46b52cb8bb2a7d5ddd0c51f7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-11-10 18:58:02 +0100

    Map: initalize map

--- src/map.js
+++ src/map.js
@@ -1,5 +1,21 @@
-module.exports = {
+const L = require('leaflet')
+
+let map
+
+const data = {
   init (callback) {
+    data.map = L.map('map').setView([51.505, -0.09], 13)
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(data.map)
+
     callback()
+  },
+
+  resize () {
+    data.map.invalidateSize()
   }
 }
+
+module.exports = data
