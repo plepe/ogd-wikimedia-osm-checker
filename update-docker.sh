32ec953c3ff3a5e377a1ea66ee297a753688be0d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-11-10 09:32:32 +0100

    Update NodeJS version

--- update-docker.sh
+++ update-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git pull
-docker pull node:14
+docker pull node:18
 docker build -t skunk/ogd-wikimedia-osm-checker .
 docker container rm -f checker
 docker run --name checker --restart=always -p 127.0.0.1:43210:8080 -v /home/skunk/code/ogd-wikimedia-osm-checker/log:/usr/src/app/log -d skunk/ogd-wikimedia-osm-checker
